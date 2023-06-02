2e2df69950e1a2c59c0552fa7f52168e3a1a9743
Raghurockystar <raghuboddu38@gmail.com>
2023-06-02 12:21:46 +0530

    fisrt commit

---
+++ src/com/sathya/calculator/calculator.java
@@ -0,0 +1,16 @@
+package com.sathya.calculator;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1=10;
+		int num2=23;
+		
+	     int result=num1-num2;
+	     System.out.println(result);
+
+	}
+
+}
